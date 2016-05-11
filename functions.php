ec27ec4479afbf57ff2fd6019ca5304ffe9a5170
Alex <alexanderddking@gmail.com>
2016-05-11 17:31:48 +1000

    mission fixing

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ include('custom-post-types/missions.php');
 
 //APIs (Endpoints)
 include('custom-apis/menu.php'); //headless/menu/[id]
-//include('custom-apis/mission.php'); //headless/missions/[slug]
+include('custom-apis/mission.php'); //headless/missions/[slug]
 include('custom-apis/pages.php'); //headless/pages/[slug]
 
 
