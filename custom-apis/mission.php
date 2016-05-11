a4cb5d8d61729ffc6cab548c69fc87cc7bfa18f9
Alex <alexanderddking@gmail.com>
2016-05-11 17:58:12 +1000

    mission fixing

--- custom-apis/mission.php
+++ custom-apis/mission.php
@@ -1,12 +1,17 @@
 <?php 
-function mission_endpoint($data ) {
-$result = $data['name'];
-        // WP_Query arguments
-$args = array (
-    'name'               	  => $data['name'],
-    'post_type'               => array('mission')
-);
-
+function mission_endpoint( $data ) {
+        $args;
+        if(isset( $data['id'])){
+            $args = array (
+                'post_type'              => array( 'mission' ),
+                'p' => $data['id']
+            );
+        } else {
+            $args = array (
+                'post_type'              => array( 'mission' ),
+            );
+        }
+$result = Array();
 // The Query
 $query = new WP_Query( $args );
 
@@ -14,22 +19,20 @@ $query = new WP_Query( $args );
 if ( $query->have_posts() ) {
     while ( $query->have_posts() ) {
         $query->the_post();
-        // $result = array( 'mission' => [get_post(get_the_id())] );
-
+    array_push($result,get_post());
     }
 } else {
-    $result = "no result";
+    $result = false;
 }
 
 // Restore original Post Data
-wp_reset_postdata();
-
+wp_reset_postdata(); 
 $result = json_decode(json_encode($result),true);
 return $result;
 }
 
 add_action( 'rest_api_init', function () {
-    register_rest_route( 'headless/', '/missions/(?P<name>[a-z0-9\-]+)', array(
+    register_rest_route( 'headless/', '/missions/(?P<id>\d+)', array(
         'methods' => 'GET',
         'callback' => 'mission_endpoint',
     ) );
