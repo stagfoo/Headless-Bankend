a13ca52fb9a870c0e8c4cf2dc024486d5ab152d6
Alex <alexanderddking@gmail.com>
2016-05-13 13:09:25 +1000

    Endpoint for all missions

--- custom-apis/mission.php
+++ custom-apis/mission.php
@@ -34,4 +34,39 @@ add_action( 'rest_api_init', function () {
         'callback' => 'mission_endpoint',
     ) );
 } );
+
+function all_mission_endpoint() {
+$result =  array();
+        // WP_Query arguments
+$args = array (
+    'post_type'               => array('mission')
+);
+
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+if ( $query->have_posts() ) {
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        array_push($result, get_post(get_the_id()));
+
+    }
+} else {
+    $result = "no result";
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+
+$result = json_decode(json_encode($result),true);
+return $result;
+}
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'headless/', '/missions', array(
+        'methods' => 'GET',
+        'callback' => 'all_mission_endpoint',
+    ) );
+} );
  ?>
